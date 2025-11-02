b5ba609a08610f6e4a954456e1c0e1cd2839de87
rishabh-os <rishabhwanjari1@gmail.com>
2025-11-02 11:59:54 +0100

    Add assets and interests

--- app/pages/interests.vue
+++ app/pages/interests.vue
@@ -15,13 +15,95 @@
 
     <UBlogPosts>
         <UBlogPost title="Nowcasting FBK"
-            description="A web app to accurately predict the next hour of weather all across Italy."
-            image="https://nuxt.com/assets/blog/nuxt-icon/cover.png" variant="soft">
+            description="A web app to accurately predict the next hour of weather all across Italy"
+            image="images/nowcasting-fbk.png" variant="soft">
 
             <template #footer>
                 <UButton to="https://nowcasting.fbk.eu" target="_blank" class="my-4 flex w-max mx-auto" size="lg"
                     variant="ghost" icon="lucide:globe" trailing-icon="lucide:external-link">
                     Website
+                </UButton>
+            </template>
+        </UBlogPost>
+
+        <UBlogPost title="Moniz" description="An app to manage and keep track of your expenses."
+            image="images/moniz.png" variant="soft">
+
+            <template #footer>
+                <UButton to="https://github.com/rishabh-os/moniz" target="_blank" class="my-4 flex w-max mx-auto"
+                    size="lg" variant="ghost" icon="lucide:github" trailing-icon="lucide:external-link">
+                    GitHub
+                </UButton>
+            </template>
+        </UBlogPost>
+
+        <UBlogPost title="React Flowcharts"
+            description="A 3 month freelance project, taken from conceptual idea to execution"
+            image="images/flowchart.png" variant="soft">
+
+            <template #footer>
+                <UButton to="https://nowcasting.fbk.eu" target="_blank" class="my-4 flex w-max mx-auto" size="lg"
+                    variant="ghost" icon="lucide:globe" trailing-icon="lucide:external-link">
+                    More info
+                </UButton>
+            </template>
+        </UBlogPost>
+
+        <UBlogPost title="Sidekick" description="A DnD companion app made with Flutter" image="images/list.png"
+            variant="soft">
+
+            <template #footer>
+                <UButton to="https://github.com/rishabh-os/dnd_sidekick" target="_blank" class="my-4 flex w-max mx-auto"
+                    size="lg" variant="ghost" icon="lucide:github" trailing-icon="lucide:external-link">
+                    GitHub
+                </UButton>
+            </template>
+        </UBlogPost>
+
+        <UBlogPost title="Mimamsa Buzzers"
+            description="An app for handling the final Mains event for Mimamsa, a national level quiz competition for undergraduates"
+            image="images/image10.png" variant="soft">
+
+            <template #footer>
+                <UButton to="https://gist.github.com/rishabh-os/41aed05521d2e207d7208b4159edec6a" target="_blank"
+                    class="my-4 flex w-max mx-auto" size="lg" variant="ghost" icon="lucide:github"
+                    trailing-icon="lucide:external-link">
+                    Gist
+                </UButton>
+            </template>
+        </UBlogPost>
+
+        <UBlogPost title="Temdex" description="A comprehensive and up-to-date encyclopedia for the game Temtem"
+            image="images/temtem_preview.jpg" variant="soft">
+
+            <template #footer>
+                <UButton to="https://github.com/rishabh-os/Temdex" target="_blank" class="my-4 flex w-max mx-auto"
+                    size="lg" variant="ghost" icon="lucide:github" trailing-icon="lucide:external-link">
+                    GitHub
+                </UButton>
+            </template>
+        </UBlogPost>
+
+        <UBlogPost title="This website!"
+            description="My personal profile on the web, made with Vue, Nuxt.js and TailwindCSS"
+            image="images/website.png" variant="soft">
+
+            <template #footer>
+                <UButton to="https://github.com/rishabh-os/gh-profile" target="_blank" class="my-4 flex w-max mx-auto"
+                    size="lg" variant="ghost" icon="lucide:github" trailing-icon="lucide:external-link">
+                    GitHub
+                </UButton>
+            </template>
+        </UBlogPost>
+
+        <UBlogPost title="Dot Files"
+            description="A collection of all my dot files for the (objectively) best desktop experience"
+            image="images/dotfiles.png" variant="soft">
+
+            <template #footer>
+                <UButton to="https://github.com/rishabh-os/Dot-Files" target="_blank" class="my-4 flex w-max mx-auto"
+                    size="lg" variant="ghost" icon="lucide:github" trailing-icon="lucide:external-link">
+                    GitHub
                 </UButton>
             </template>
         </UBlogPost>
